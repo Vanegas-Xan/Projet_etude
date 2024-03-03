7c51a033d9a4e146d28480e14d1804000ef5e159
Vanegas-Xan <78452178+Vanegas-Xan@users.noreply.github.com>
2024-03-02 22:38:56 -0500

    Add files via upload

---
+++ js/carrousel.js
@@ -0,0 +1,17 @@
+let slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Change l'image toutes les 2 secondes
+}
